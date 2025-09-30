73d88f927386985e926e6d29caee1011a76c9242
vl07477 <vlada0624@gmail.com>
2025-09-30 22:43:03 +0500

    HW 4 week (interface\implements)

--- module_4/src/Homework/easy/Main.java
+++ module_4/src/Homework/easy/Main.java
@@ -3,6 +3,7 @@ package Homework.easy;
 public class Main {
     public static void main(String[] args) {
         Printer<String> printer = new StringPrinter();
-        printer.print("Privet, mir (week4)!");
+        printer.print("Privet, mir!");
+        printer.print("remake HW (week4)!");
     }
 }
