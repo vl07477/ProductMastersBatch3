cbe889126422ed95a928926854cc5eee4f263750
vl07477 <vlada0624@gmail.com>
2025-10-04 20:30:37 +0500

    HW 5 week (enum) v2 with enum list (+medium lv)

--- module_5/src/Homework/five/medium/DayOfWeek.java
+++ module_5/src/Homework/five/medium/DayOfWeek.java
@@ -1,4 +1,28 @@
 package Homework.five.medium;
 
+
 public enum DayOfWeek {
+    Monday("Понедельник", 1),
+    Tuesday("Вторник", 2),
+    Wednesday("Среда", 3),
+    Thursday("Четверг", 4),
+    Friday("Пятница", 5),
+    Saturday("Суббота", 6),
+    Sunday("Воскресенье", 7); // ← ← ← ← вот тут ОБЯЗАТЕЛЬНО точка с запятой!
+
+    private final String ruName;
+    private final int dayNumber;
+
+    DayOfWeek(String ruName, int dayNumber) {
+        this.ruName = ruName;
+        this.dayNumber = dayNumber;
+    }
+
+    public String getRuName() {
+        return ruName;
+    }
+
+    public int getDayNumber() {
+        return dayNumber;
+    }
 }
--- module_5/src/Homework/five/medium/Main.java
+++ module_5/src/Homework/five/medium/Main.java
@@ -1,12 +1,25 @@
 package Homework.five.medium;
-
+import java.util.ArrayList;
+import java.util.List;
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        List<DayOfWeek> days = new ArrayList<>();
+        for (DayOfWeek day : DayOfWeek.values()) {
+            days.add(day);
+        }
 
-  }
-  public static boolean isWeekend(DayOfWeek day) {
-    return false;
-  }
+        System.out.println("Days: ");
+        for (DayOfWeek day : days) {
+            System.out.println(day.getRuName());
+        }
+        DayOfWeek today = DayOfWeek.Saturday;
+        System.out.println("Сегодня: " + today.getRuName());
+        System.out.println("Выходной? " + isWeekend(today));
+    }
 
+    public static boolean isWeekend(DayOfWeek day) {
+        int num = day.getDayNumber();
+        return num == 6 || num == 7;
+    }
 }
