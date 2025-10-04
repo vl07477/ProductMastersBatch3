db4ffd908382cdb4e48530e147826f1580420343
vl07477 <vlada0624@gmail.com>
2025-10-04 19:42:41 +0500

    HW 5 week (enum) v1 without enum list

--- module_5/src/Homework/five/easy/Main.java
+++ module_5/src/Homework/five/easy/Main.java
@@ -4,8 +4,7 @@ public class Main {
     public static void main(String[] args) {
         DayOfWeek today = DayOfWeek.SATURDAY; //from enum
         System.out.println("Today: " + today);
-        for (DayOfWeek day: DayOfWeek.values() ){
-            System.out.println(day);
+       // for (DayOfWeek day: DayOfWeek.values() ){
+           // System.out.println(day);
         }
     }
-}
